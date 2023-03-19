c5f5744652805883b3020fdb2b48f364d1df8131
Ismael Carvajal <ismaelcarvajal@outlook.com>
2023-03-19 14:07:22 -0500

    Agrego miLista.js

---
+++ miLista.js
@@ -0,0 +1,15 @@
+const miLista = new Array(5);
+
+miLista[0] = 'Ismael Carvajal';
+miLista[1] = 26;
+miLista[2] = true;
+miLista[3] = new Date("april 7 1996");
+const libro = {
+    titulo: 'Cien años de soledad',
+    autor: 'Gabriel Garcia Marquez',
+    fecha: new Date("march 05 1970"),
+    url: 'https://www.secst.cl/upfiles/documentos/02072019_916am_5d1b755b8c54f.pdf'
+}
+miLista[4] = libro;
+
+console.log(miLista);
